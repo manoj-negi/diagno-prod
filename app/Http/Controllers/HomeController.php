c0f67086e2a20881e82191a6d2a9bcb5e528fa6e
Rahul <sharma4271.rs@gmail.com>
2024-10-28 10:30:10 +0530

    csv

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -91,7 +91,7 @@ class HomeController extends Controller
     //     }
     //  }
 
-    public function (Request $request)
+    public function importCsv(Request $request)
 {
     $request->validate([
         'file' => 'required|file|mimes:csv,txt',
